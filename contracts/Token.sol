926baa7f23b8d9a280d5f32ba2330eaab046cab6
khokharkeyur <keyurkhokhar04@gmail.com>
2025-02-09 09:37:07 +0530

    Add initial Token contract with basic functionality and total supply

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "HardHat Token";
+    string public symbol = "HHT";
+    uint public totalSupply = 1000000;
+    address public owner;
+    mapping(address => uint) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+    function balancesOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+}
